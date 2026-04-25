1bf296e7ccf4f9042fb6b2bc1b012ef733239d76
Rashdeen <it25103637@my.sliit.lk>
2026-04-25 10:00:00 +0530

    Initial commit: Add Maven and project structure

---
+++ src/main/java/com/rental/VehicleRentalApplication.java
@@ -0,0 +1,23 @@
+/**
+ * Component 03: Core Application
+ * @author Rashdeen (it25103637@my.sliit.lk)
+ */
+package com.rental;
+
+import com.rental.nichala.*;
+import com.rental.abhishek.*;
+import com.rental.rashdeen.*;
+import com.rental.nisal.*;
+import com.rental.sandaru.*;
+import com.rental.sithika.*;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleRentalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VehicleRentalApplication.class, args);
+    }
+}
