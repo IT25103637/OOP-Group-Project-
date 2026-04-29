3845bdf4a6dc2900e9bff13df18a4ad6e5ea9c4b
Nichala <it25102056@my.sliit.lk>
2026-04-29 10:10:00 +0530

    Refactor models and add validation

---
+++ src/main/java/com/rental/nichala/PaymentStatus.java
@@ -0,0 +1,17 @@
+/**
+ * Component 01: Models & Entities
+ * @author Nichala (it25102056@my.sliit.lk)
+ */
+package com.rental.nichala;
+
+import com.rental.nichala.*;
+import com.rental.abhishek.*;
+import com.rental.rashdeen.*;
+import com.rental.nisal.*;
+import com.rental.sandaru.*;
+import com.rental.sithika.*;
+
+public enum PaymentStatus {
+    PENDING,
+    PAID
+}
